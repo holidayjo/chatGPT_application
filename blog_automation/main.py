e787de41c59532245559b1100a88056f80db0bbb
Holiday_Jo <ossam1996@gmail.com>
2023-02-24 23:29:44 +0900

    Update main.py

--- blog_automation/main.py
+++ blog_automation/main.py
@@ -2,14 +2,14 @@ import utils
 import pandas as pd
 
 # Pick the topic sentence.```
-words = "To make a kid to read books."
+words = "How to move furnitures without breaking"
 
 
 generated_articles = utils.article_generator(words, max_tokens=2048)
 # print(generated_articles)
 # utils.saving_csv(words, generated_articles)
 translated = utils.translator(generated_articles)
-# print(translated)
+print(translated)
 
-utils.posting_tistory(title=words, content=translated)
+# utils.posting_tistory(title=words, content=translated)
 
